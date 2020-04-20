5008a6169e22613583d7f480a9f6725a7abf397a
alsino <iamahorse@gmail.com>
2020-04-20 20:17:40 +0200

    Basic setup

---
+++ src/components/Intro.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { DATA } from '../store.js';
+  import { onMount, beforeUpdate } from 'svelte';
+
+
+  onMount(() => {
+		console.log($DATA);
+	});
+
+
+</script>
+
+
+<style lang="scss">
+ @import "./style/theme.scss";
+
+ h1 {
+  color: $main-color;
+ }
+
+</style>
+
+
+<div>{JSON.stringify($DATA)}</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Intro from '../components/Intro.svelte';
+</script>
+
+<svelte:head>
+	<title>Visitmytent network</title>
+</svelte:head>
+
+
+<Intro/>
+
