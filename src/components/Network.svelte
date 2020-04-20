9e889e927dbc6eee5c83ef8f581d3708c518996c
alsino <iamahorse@gmail.com>
2020-04-20 20:49:00 +0200

    Basic setup w/ d3

--- src/components/Network.svelte
+++ src/components/Network.svelte
@@ -1,21 +1,29 @@
 <script>
   import { DATA } from '../store.js';
   import { onMount, beforeUpdate } from 'svelte';
+  import * as d3 from "d3";
 
 
   onMount(() => {
-		console.log($DATA);
-	});
-
-
+    console.log($DATA);
+  });
+  
 </script>
 
 
 <style lang="scss">
  @import "./style/theme.scss";
 
- h1 {
-  color: $color-primary;
+ #network {
+   width: 100%;
+   height: 600px;
+   background: $color-primary;
+   margin-top: $margin-small;
  }
 
 </style>
+<br>
+Example: D3 w/ svelte
+<div>https://svelte.dev/repl/01a5774b53e9416584428c025668407b?version=3.15.0</div>
+<div id="network">Network here</div>
+
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 import networkData from './data/artists_200113.json';
-// import * as d3 from "d3";
+
 
 export const DATA = writable(undefined);
 DATA.set(networkData);
