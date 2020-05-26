233b8e1f3bddb72a65450dca6c0423cefd6d7edb
Stephanie Neumann <ahoi@stephanieneumann.com>
2020-05-26 17:14:37 +0200

    update artist-datei adressen weg + disziplinen

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 import geoData from './data/geodata.json';
-import networkData from './data/artists_200525.json';
+import networkData from './data/artists_200526.json';
 
 export const VIEW = writable(undefined);
 VIEW.set("Network");
