3ce18dfc00895921ab61ab0f58b42c56231ecbb5
alsino <iamahorse@gmail.com>
2020-05-26 20:48:59 +0200

    Night mode

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,5 +1,8 @@
 <script>
-  import { VIEW } from '../store.js';
+  import { VIEWMODE, COLORS } from '../store.js';
+
+  $: colorScheme = $VIEWMODE == "Day" ? $COLORS.day : $COLORS.night;
+
 </script>
 
 
@@ -22,7 +25,7 @@
 
 </style>
 
-<div>
+<div style="background:{colorScheme.background}; color:{colorScheme.textDefault}">
   <h2>Artistellar</h2>
   <em>Exploring Artists’ Networks</em> – Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>. A visualization by <a href="https://stephanieneumann.com/" target="_blank">Stephanie Neumann</a> and <a href="https://visitmytent.com/?p=11325" target="_blank">Alsino Skowronnek</a> – Berlin, 2020. 
 </div>
