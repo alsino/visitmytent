634dd156057e2b0cfa8df781b10f85ba7f00704c
alsino <iamahorse@gmail.com>
2020-07-30 13:11:40 +0200

    Info hideable

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,5 +1,8 @@
 <script>
   import { VIEWMODE, COLORS } from '../store.js';
+  import { slide } from 'svelte/transition';
+
+  let introVisible = true;
 
   $: colorScheme = $VIEWMODE == "Day" ? $COLORS.day : $COLORS.night;
 
@@ -16,15 +19,13 @@
  div {
    color: $color-text;
    margin-top: 0;
-   margin-bottom: $margin-small;
-  //  width: 80%;
-  //  width: 100%;
    transition: all 2s;
  }
 
  .row {
    display: flex;
    justify-content: flex-start;
+   position: relative;
  }
 
 .column {
@@ -32,20 +33,41 @@
   margin-right: $margin-small * 1.5; 
 }
 
+.toggle {
+  text-transform: uppercase;
+  color: $color-primary;
+  font-size: 0.7em;
+  margin: 1em 0;
+}
+
+
+
 
 
 </style>
 
 <div style="background:{colorScheme.background}; color:{colorScheme.textDefault}">
-  <h2 class="heading" style="color:{colorScheme.textHeading}">Artistellar &mdash;</h2>
-  <div style="color:{colorScheme.textDefault}"><em>Exploring Artists’ Networks</em></div>
-  <div class="row">
-    <div class="column" style="color:{colorScheme.textDefault}">Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>.</div>
-    <div class="column" style="color:{colorScheme.textDefault}">Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>.</div>
-    <div class="column" style="color:{colorScheme.textDefault}">Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>.</div>
-   
-    <!-- <div><em>Exploring Artists’ Networks</em> – Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>. A visualization by <a href="https://stephanieneumann.com/" target="_blank">Stephanie Neumann</a> and <a href="https://visitmytent.com/?p=11325" target="_blank">Alsino Skowronnek</a> – Berlin, 2020.</div> -->
+  <h2 class="heading" style="color:{colorScheme.textHeading}">Artistellar</h2>
+  <div style="color:{colorScheme.textDefault}">
+    <em>Exploring Artists’ Networks</em>
+    
   </div>
+
+  {#if introVisible}
+    <div class="row" in:slide="{{ y: 200, duration: 1000 }}" out:slide="{{ y: 200, duration: 1000 }}">
+      <div class="column" style="color:{colorScheme.textDefault}">Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>.</div>
+      <div class="column" style="color:{colorScheme.textDefault}">Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>.</div>
+      <div class="column" style="color:{colorScheme.textDefault}">Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>.</div>
+    </div>
+    
+  {/if}
+  <div 
+    class="toggle"
+    on:click={()=>{introVisible = !introVisible}}
+    >{introVisible ?  "Hide info" : "Show info"}
+    </div>
    
 </div>
+
+<!-- <div><em>Exploring Artists’ Networks</em> – Artistellar visualizes studio locations as well as connections between contemporary artists working in various fields, creating an inside view into the networks of selected artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>. A visualization by <a href="https://stephanieneumann.com/" target="_blank">Stephanie Neumann</a> and <a href="https://visitmytent.com/?p=11325" target="_blank">Alsino Skowronnek</a> – Berlin, 2020.</div> -->
 
