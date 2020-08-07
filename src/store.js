684eca7c8d4139a313d48e24cd27b96eca4d30e8
Stephanie Neumann <ahoi@stephanieneumann.com>
2020-08-07 18:35:28 +0200

    artist info update

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import { goto } from '@sapper/app';
 import geoData from './data/geodata.json';
-import networkData from './data/artists_200803.json';
+import networkData from './data/artists_200807.json';
 
 export const VIEW = writable(undefined);
 VIEW.set("Map");
