475413bce5d08226a1ebdb138b37576547766bec
alsino <iamahorse@gmail.com>
2020-11-25 12:07:14 +0100

    blotter simple text added 2

--- src/routes/[artist].svelte
+++ src/routes/[artist].svelte
@@ -34,57 +34,37 @@
     $selectedArtist = startName;
 
 
-  // // BLOTTER - Example 1
-  //   var text = new Blotter.Text("observation", {
-  //     family : "'EB Garamond', serif",
-  //     size : 27,
-  //     fill : "#202020"
-  //   });
+    // BLOTTER - Example 2
+    let text = new Blotter.Text("observation", {
+      family : "'EB Garamond', serif",
+      size : 27,
+      fill : "#171717",
+      paddingLeft : 40,
+      paddingRight : 40
+    });
 
-  // // var material = new Blotter.Material();
-  // // var material = new Blotter.ChannelSplitMaterial();
-  // const material = new Blotter.LiquidDistortMaterial();
+    let material = new Blotter.LiquidDistortMaterial();
 
-  // var blotter = new Blotter(material, {
-  //   texts : text
-  // });
+    // Play with the value for uSpeed. Lower values slow
+    // down animation, while higher values speed it up. At
+    // a speed of 0.0, animation is stopped entirely.
+    material.uniforms.uSpeed.value = 0.25;
 
-  // var elem = document.getElementById("plain-text");
-  // var scope = blotter.forText(text);
+    // Try uncommenting the following line to play with
+    // the "volatility" of the effect. Higher values here will
+    // produce more dramatic changes in the appearance of your
+    // text as it animates, but you will likely want to keep
+    // the value below 1.0.
+    // material.uniforms.uVolatility.value = 0.30;
 
-  // scope.appendTo(elem);
+    let blotter = new Blotter(material, {
+      texts : text
+    });
 
-  // BLOTTER - Example 2
-var text = new Blotter.Text("observation", {
-  family : "'EB Garamond', serif",
-  size : 27,
-  fill : "#171717",
-  paddingLeft : 40,
-  paddingRight : 40
-});
+    let elem = document.getElementById("plain-text");
+    let scope = blotter.forText(text);
 
-var material = new Blotter.LiquidDistortMaterial();
-
-// Play with the value for uSpeed. Lower values slow
-// down animation, while higher values speed it up. At
-// a speed of 0.0, animation is stopped entirely.
-material.uniforms.uSpeed.value = 0.25;
-
-// Try uncommenting the following line to play with
-// the "volatility" of the effect. Higher values here will
-// produce more dramatic changes in the appearance of your
-// text as it animates, but you will likely want to keep
-// the value below 1.0.
-//material.uniforms.uVolatility.value = 0.30;
-
-var blotter = new Blotter(material, {
-  texts : text
-});
-
-var elem = document.getElementById("plain-text");
-var scope = blotter.forText(text);
-
-scope.appendTo(elem);
+    scope.appendTo(elem);
 
 
   });
