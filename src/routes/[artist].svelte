b0f36acec0534d13d0958fbb7e037d724361bc5e
alsino <iamahorse@gmail.com>
2020-12-02 12:35:39 +0100

    reactive text on two lines

--- src/routes/[artist].svelte
+++ src/routes/[artist].svelte
@@ -16,14 +16,12 @@
 
   export let artist;
 
-  let textWidth = 300;
-  let textHeight = 500;
-
-  let blotterText;
+  let text1, text2;
+  let elem1, elem2;
+  let scope1, scope2;
   let material;
   let blotter;
-  let elem;
-  let scope;
+  
 
   $: colorScheme = $VIEWMODE == "Day" ? $COLORS.day : $COLORS.night;
 
@@ -36,23 +34,18 @@
   })[0];
 
 
-  $: updateBlotterText = function(){
-      // blotterText.value = $selectedArtistDetails.questions.why;
-      blotterText.value = $selectedArtistDetails.name;
-      blotterText.needsUpdate = true;
-      // console.log(blotterText);
-  }
-
-
   beforeUpdate(() => {
 
-    if ($selectedArtist) {
+    if ($selectedArtist == "Jeewi Lee") {
 
       if ($selectedArtistDetails.questions.why != " ") {
 
-        if (blotterText) {
-          blotterText.value = $selectedArtistDetails.questions.why;
-          blotterText.needsUpdate = true;
+        if (text1 && text2) {
+          text1.value = $selectedArtistDetails.questions.why[0];
+          text1.needsUpdate = true;
+
+          text2.value = $selectedArtistDetails.questions.why[1];
+          text2.needsUpdate = true;
         }
 
       }
@@ -67,15 +60,18 @@
     $selectedArtist = startName;
     $selectedArtistDetails = startDetails;
 
-    blotterText = new Blotter.Text("", {
+    let styleProperties = {
         family : "'EB Garamond', serif",
         size : 40,
         fill : "#000",
         paddingLeft : 40,
         paddingRight : 40
-      });
-    
+    };
 
+    text1 = new Blotter.Text("", styleProperties);
+    text2= new Blotter.Text("", styleProperties);
+
+    
     material = new Blotter.LiquidDistortMaterial();
 
     // Play with the value for uSpeed. Lower values slow
@@ -91,12 +87,19 @@
     material.uniforms.uVolatility.value = 0.06;
 
     blotter = new Blotter(material, {
-      texts : blotterText
+      texts : [text1, text2]
     });
 
-    elem = document.getElementById("plain-text");
-    scope = blotter.forText(blotterText);
-    scope.appendTo(elem);
+    elem1 = document.getElementById("plain-text");
+    elem2 = document.getElementById("plain-text-2");
+  
+    scope1 = blotter.forText(text1);
+    scope1.appendTo(elem1);
+
+    scope2 = blotter.forText(text2);
+    scope2.appendTo(elem2);
+
+
 
   });
 
@@ -106,25 +109,18 @@
 
 <ArtistInfo/>
 
-<!-- {#if $selectedArtistDetails}
- 
-  {#if $selectedArtistDetails.questions}
-    <div 
-      id="text" 
-      style="top: {$MOUSE.y - 30}px; left:{$MOUSE.x}px; width:{textWidth}px; height:{textHeight}px; color:{colorScheme.circleSelected};"
-      >
-      {$selectedArtistDetails.questions.why}
-    </div>
-  {/if}
-{/if} -->
-
 
   <div 
   id="plain-text"
-  style="top: {$MOUSE.y - 30}px; left:{$MOUSE.x}px; width:{textWidth}px; height:{textHeight}px; color:{colorScheme.circleSelected};"
+  style="top: {$MOUSE.y - 40}px; left:{$MOUSE.x}px; color:{colorScheme.circleSelected};"
   ></div>
 
-  <button on:click={updateBlotterText}>click</button>
+  <div 
+  id="plain-text-2"
+  style="top: {$MOUSE.y }px; left:{$MOUSE.x}px; color:{colorScheme.circleSelected};"
+  ></div>
+
+  <!-- <button on:click={updatetext1}>click</button> -->
 
 
 
@@ -141,6 +137,16 @@
       pointer-events: none;
       display: block;
       background-color: transparent;
+      width: 500px;
+  }
+
+  #plain-text-2 {
+      font-size: 1em;
+      position: absolute;
+      pointer-events: none;
+      display: block;
+      background-color: transparent;
+      width: 500px;
   }
   
   
