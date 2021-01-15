3c029bafca4d78b7bb5497d10d6b6c02dde0fbc3
Stephanie Neumann <ahoi@stephanieneumann.com>
2021-01-15 14:07:26 +0100

    links corrected a-g

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import { goto } from '@sapper/app';
 import geoData from './data/geodata.json';
-import networkData from './data/artists_201228.json';
+import networkData from './data/artists_210115.json';
 
 export const WWIDTH = writable(undefined);
 
