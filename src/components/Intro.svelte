8184c6c9895a96c69b09b6f85f086ed0b1ca6c6e
alsino <iamahorse@gmail.com>
2021-02-08 11:41:21 +0100

    new intro text edit 2

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -59,9 +59,9 @@
     <div class="row" in:slide="{{ y: 200, duration: 1000 }}" out:slide="{{ y: 200, duration: 1000 }}">
     <div class="column" style="color:{colorScheme.textDefault}">visitmyorbit is a portal into the life of contemporary artists working in various fields. The site shows artists' motivations, studio locations as well as {links.length} connections among them – creating an inside view into the networks of {nodes.length} artists presented on <a href="https://visitmytent.com/" target="_blank">visitmytent</a>.</div>
       
-      <div class="column" style="color:{colorScheme.textDefault}">Highlighting individual artists points out their relation and closeness to other artists – socially in the network and physically on the map – giving a glimpse into the different artists' milieus – predominantly in Berlin. Once you're curious about an artist, you can dive into the respective</div>
+      <div class="column" style="color:{colorScheme.textDefault}">Highlighting individual artists points out their relation and closeness to other artists – socially in the network and physically on the map – giving a glimpse into the different artists' milieus – predominantly in Berlin. Additionally you can deep dive into individual studio visits and interviews.</div>
       
-      <div class="column" style="color:{colorScheme.textDefault}">studio visit and interview. All data was collected manually and evaluated in a constant dialogue with the artists over a period of more than one year. visitmyorbit is a visualization by <a href="https://stephanieneumann.com/" target="_blank">Stephanie Neumann</a> and <a target="_blank" href="https://alsino.io/">Alsino Skowronnek</a> crafted in Berlin, 2021.</div>
+      <div class="column" style="color:{colorScheme.textDefault}">All data was collected manually and evaluated in a constant dialogue with the artists over a period of more than one year. visitmyorbit is a visualization by <a href="https://stephanieneumann.com/" target="_blank">Stephanie Neumann</a> and <a target="_blank" href="https://alsino.io/">Alsino Skowronnek</a> crafted in Berlin, 2021.</div>
     </div>
     
   {/if}
