e26fdde773ee5401490a4d86ec0d31d23d77e518
alsino <iamahorse@gmail.com>
2021-02-09 09:59:47 +0100

    social tags added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,10 @@
 <svelte:head>
 	<title>visitmyorbit</title>
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@alsinosko" />
+	<meta name="twitter:creator" content="@alsinosko" />
+	<meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
+	<meta property="og:title" content="visitmyorbit" />
+	<meta property="og:description" content="Exploring Artists' Networks" />
+	<meta property="og:image" content="/social.png" />
 </svelte:head>
